2387f741679741b3b114e47120a2023a04e2e6b4
StephaneVogrig <stephane.vogrig@gmail.com>
2025-05-05 01:04:29 +0200

    reforge ex00 pour coller au sujet

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 21:25:01 by svogrig           #+#    #+#             */
-/*   Updated: 2025/03/24 02:57:47 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/05/05 01:01:51 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ ScalarConverter::ScalarConverter(void)
 
 ScalarConverter::ScalarConverter(const ScalarConverter & toCopy)
 {
-	*this = toCopy;
+	(void)toCopy;
 }
 
 /* destructor ----------------------------------------------------------------*/
@@ -31,95 +31,109 @@ ScalarConverter::~ScalarConverter(void)
 
 ScalarConverter & ScalarConverter::operator = (const ScalarConverter & toAssign)
 {
-	if (this == &toAssign)
-		return *this;
+	(void)toAssign;
 	return *this;
 }
 
-/* utilities -----------------------------------------------------------------*/
-
-/*
-	Checking for NaN bit patterns in C++
-	https://codereview.stackexchange.com/questions/272093/checking-for-nan-bit-patterns-in-c
-	A double (IEEE 754) is a nan if:
-	- all bits  of exposant are 1
-	- mantisse is not 0
-
-*/
-static bool isNan(double d)
+void ScalarConverter::convert(const std::string & str)
 {
-	union U {unsigned long long ui64; double f64;} x;
-	x.f64 = d;
-	return (x.ui64 & 0x7FFFFFFFFFFFFFFFu) >= 0x7FF0000000000001u;
-}
-
-static bool isChar(const std::string & literal)
-{
-	if (literal.length() != 1)
-		return false;
-	if (literal[0] < 32 || literal[0] > 126)
-		return false;
-	if (isdigit(literal[0]))
-		return false;
-	return true;
-}
-
-static bool isInvalidLiteral(const std::string & literal, char * endptr)
-{
-	if (endptr == literal.c_str())
-		return true;
-
-	const char * endstr = literal.c_str() + literal.length() - 1;
-	if (endptr == endstr && *endptr == 'f')
-		return false;
-
-	if (*endptr != '\0')
-		return true;
-
-	return false;
-}
-
-void ScalarConverter::convert(const std::string & literal)
-{
-	char * endptr = NULL;
-	double d;
-	if (isChar(literal))
-		d = static_cast<double>(literal[0]);
+	if (isChar(str))
+		convertFromChar(str);
+	else if (isInt(str))
+		convertFromInt(str);
+	else if (isFloat(str))
+		convertFromFloat(str);
+	else if (isDouble(str))
+		convertFromDouble(str);
 	else
 	{
-		d = strtod(literal.c_str(), &endptr);
-		if (isInvalidLiteral(literal, endptr))
-		{
-			std::cout << "char: impossible" << std::endl;
-			std::cout << "int: impossible" << std::endl;
-			std::cout << "float: impossible" << std::endl;
-			std::cout << "double: impossible" << std::endl;
-			return ;
-		}
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
 	}
-
-	/* display in char */
-	std::cout << "char: ";
-	if ((d >= 0 && d < 32) || d == 127)
-		std::cout << "Non displayable" << std::endl;
-	else if (d > 31 && d < 127)
-		std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
-	else
-		std::cout << "impossible" << std::endl;
-
-	/* display in int */
-	std::cout << "int: ";
-	if (d > INT_MAX || d < INT_MIN || isNan(d))
-		std::cout << "impossible" << std::endl;
-	else
-		std::cout << static_cast<int>(d) << std::endl;
-
-	/* display in float */
-	std::cout << "float: ";
-	std::cout  << std::fixed << std::setprecision(1);
-	std::cout << static_cast<float>(d) << "f" << std::endl;
-
-	/* display in float */
-	std::cout << "double: " << d << std::endl;
-
 }
+
+bool ScalarConverter::isChar(const std::string & str)
+{
+	return str.length() == 1 && !isdigit(str[0]);
+}
+
+bool ScalarConverter::isInt(const std::string & str)
+{
+	char * endptr;
+	errno = 0;
+	long value = strtol(str.c_str(), &endptr, BASE_10);
+	return errno == 0 && *str.begin() != '\0' && *endptr == '\0' && value >= INT_MIN && value <= INT_MAX;
+}
+
+bool ScalarConverter::isFloat(const std::string & str)
+{
+	if (str.empty() || *(str.end() - 1) != 'f')
+		return false;
+	std::string tmp = str.substr(0, str.length() - 1);
+	char * endptr;
+	errno = 0;
+	strtof(tmp.c_str(), &endptr);
+	return errno == 0 && *endptr == '\0';
+}
+
+bool ScalarConverter::isDouble(const std::string & str)
+{
+	if (str.empty())
+		return false;
+	char * endptr;
+	errno = 0;
+	double value = strtod(str.c_str(), &endptr);
+	std::cout << "value: " << value << std::endl;
+	return errno == 0 && *endptr == '\0';
+}
+
+void ScalarConverter::convertFromChar(const std::string & str)
+{
+	char value = *str.begin();
+	std::cout << "char: " << value << std::endl;
+	std::cout << "int: " << static_cast<int>(value) << std::endl;
+	std::cout << "float: " << static_cast<float>(value) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(value) << ".0" << std::endl;
+}
+
+void ScalarConverter::convertFromInt(const std::string & str)
+{
+	int value = atoi(str.c_str());
+	printChar<int>(value);
+	std::cout << "int: " << value << std::endl;
+	std::cout << "float: " << static_cast<float>(value) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(value) << ".0" << std::endl;
+}
+
+void ScalarConverter::convertFromFloat(const std::string & str)
+{
+	float value = strtof(str.c_str(), NULL);
+	printChar<float>(value);
+	printInt<float>(value);
+	printFloat(value);
+	printDouble(static_cast<float>(value));
+}
+
+void ScalarConverter::convertFromDouble(const std::string & str)
+{
+	double value = strtod(str.c_str(), NULL);
+	printChar<double>(value);
+	printInt<double>(value);
+	printFloat(static_cast<float>(value));
+	printDouble(value);
+}
+
+void ScalarConverter::printFloat(float value)
+{
+	int nbr_decimal = nbrDecimal<float>(value);
+	std::cout << std::fixed << std::setprecision(nbr_decimal) << "float: " << value << 'f' << std::endl;
+};
+
+void ScalarConverter::printDouble(double value)
+{
+	int nbr_decimal = nbrDecimal<double>(value);
+	std::cout << std::fixed << std::setprecision(nbr_decimal) << "double: " << value << std::endl;
+};
+
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 16:39:36 by svogrig           #+#    #+#             */
-/*   Updated: 2025/03/24 02:26:15 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/05/05 01:02:14 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,74 @@
 # include <cstdlib>
 # include <string>
 # include <iomanip>
+# include <sstream>
 # include <climits>
+# include <cerrno>
 # include <cfloat>
+# include <cmath>
+# include <limits>
+
+# define BASE_10 10
+
+template <typename valueType>
+bool is_nan(valueType value)
+{
+	return value != value;
+}
+
+template <typename valueType>
+bool is_inf(valueType value)
+{
+	return value == value + 1;
+}
+
+template <typename valueType>
+int nbrDecimal(valueType value)
+{
+	if (is_nan(value) || is_inf(value))
+		return 0;
+	valueType decimal_part = std::abs(std::fmod(value, static_cast<valueType>(1.0)));
+	int nbr_decimal = 0;
+	int precision = 0;
+	int nbr_zero_at_end = 0;
+	while (decimal_part != static_cast<valueType>(0.0) && precision <= std::numeric_limits<valueType>::digits10)
+	{
+		decimal_part *= static_cast<valueType>(10);
+		int int_part = static_cast<int>(decimal_part);
+		if (precision != 0 || int_part != 0)
+			precision++;
+		if (int_part != 0)
+			nbr_zero_at_end = 0;
+		else
+			nbr_zero_at_end++;
+		decimal_part = std::fmod(decimal_part, static_cast<valueType>(1.0));
+		nbr_decimal++;
+	}
+	nbr_decimal -= nbr_zero_at_end;
+	std::cout << "nbr_decimal: " << nbr_decimal << std::endl;
+	return nbr_decimal;
+};
+
+template <typename valueType>
+void printChar(valueType value)
+{
+	if (value >= static_cast<valueType>(32) && value < static_cast<valueType>(128))
+		std::cout << "char: '" << static_cast<char>(value) << "'" << std::endl;
+	else if (is_nan(value) || value < static_cast<valueType>(0) || value >= static_cast<valueType>(129))
+		std::cout << "char: impossible" << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+};
+
+template <typename valueType>
+void printInt(valueType value)
+{
+	if (value >= static_cast<valueType>(INT_MIN) && value <= static_cast<valueType>(INT_MAX))
+		std::cout << "int: " << static_cast<int>(value) << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+};
+
 
 class ScalarConverter
 {
@@ -30,10 +96,21 @@ class ScalarConverter
 
 		ScalarConverter(void);
 		ScalarConverter(const ScalarConverter & toCopy);
-
 		~ScalarConverter(void);
-
 		ScalarConverter & operator = (const ScalarConverter & toAssign);
+
+		static bool isChar(const std::string & str);
+		static bool isInt(const std::string & str);
+		static bool isFloat(const std::string & str);
+		static bool isDouble(const std::string & str);
+
+		static void convertFromChar(const std::string & str);
+		static void convertFromInt(const std::string & str);
+		static void convertFromFloat(const std::string & str);
+		static void convertFromDouble(const std::string & str);
+
+		static void printFloat(float value);
+		static void printDouble(double value);
 
 };
 
